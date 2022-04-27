71ce36c5a7aba8af1cba6acdfbc4a34aad930a50
linwie <286645611@qq.com>
2022-04-27 18:37:57 +0800

    feat: 项目初始化

---
+++ src/views/home/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>HOME</h1>
+    <HelloWorld :msg="msg"></HelloWorld>
+  </div>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from "@/components/HelloWorld.vue"
+import request from "@/service"
+import { onMounted } from "vue"
+const msg = "你好"
+onMounted(async () => {
+  const res = await request.get("/get", {})
+  console.log("res", res)
+
+  const post = await request.post("/post", { params: 123 })
+  console.log("post", post)
+})
+</script>
+
+<style lang="less" scoped></style>
