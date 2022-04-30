dee0ac2e4b219ee09298b011a09ff1737cbcf63d
邓林威 <denglw4@midea.com>
2022-04-30 16:20:35 +0800

    增加引用和解决警告

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import path from "path"
+import viteCompression from "vite-plugin-compression"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -32,11 +33,15 @@ export default defineConfig({
     proxy: {},
   },
   build: {
+    // 打包删除console 和 debugger
     terserOptions: {
       compress: {
+        // eslint-disable-next-line camelcase
         drop_console: true,
+        // eslint-disable-next-line camelcase
         drop_debugger: true,
       },
     },
+    minify: "terser",
   },
 })
